03764bfa0b92ed13278867e3a5566d7c81394b6f
root <root@project-dev.pensoft.local>
2022-02-28 16:19:38 +0200

    stable

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,12 +37,12 @@ $(document).ready(function() {
 	$('body').on('click', '.work_packages .accordion-toggle', function () {
 		if ($(this).next(".accordion-content").is(':visible')) {
 			$(this).next(".accordion-content").slideUp(300);
-			$(this).children(".plusminus").addClass('plus');
-			$(this).children(".plusminus").removeClass('minus');
+			$(this).children().find(".plusminus").text('+');
+			$(this).children(".plusminus").html('<span class="plus"></span>');
 		} else {
 			$(this).next(".accordion-content").slideDown(300);
-			$(this).children(".plusminus").removeClass('plus');
-			$(this).children(".plusminus").addClass('minus');
+			$(this).children().find(".plusminus").text('-');
+			$(this).children(".plusminus").html('<span class="minus"></span>');
 		}
 	});
 
@@ -70,44 +70,129 @@ $(document).ready(function() {
 
 	});
 
+	//
+	// $('.content-wrapper.news .content img').each(function(){
+	// 	$(this).attr('id', 'myImg');
+	// 	$(this).addClass('myImages');
+	// });
+	//
+	// $('.content-wrapper.news').after('<div id=\"myModal\" class=\"modal\">\n' +
+	// 	'  <span class=\"close_modal\">&times;</span>\n' +
+	// 	'  <img class=\"modal-content\" id=\"img01\">\n' +
+	// 	'  <div id=\"caption\"></div>\n' +
+	// 	'</div>');
 
-	$('.content-wrapper.news .content img').each(function(){
-		$(this).attr('id', 'myImg');
-		$(this).addClass('myImages');
+
+	$('.tabs').each(function(){
+		// For each set of tabs, we want to keep track of
+		// which tab is active and its associated content
+		var $active, $content, $links = $(this).find('a');
+		var speed = "fast";
+		var activeTab = $(location.hash);
+		// If the location.hash matches one of the links, use that as the active tab.
+		// If no match is found, use the first link as the initial active tab.
+		$active = $($links.filter("[href=\'"+location.hash+"\']")[0] || $links[0]);
+
+		$active.addClass('active');
+
+		$content = $($active[0].hash);
+
+		// Hide the remaining content
+		$links.not($active).each(function () {
+			$(this.hash).hide();
+		});
+
+		if(activeTab.length){
+			$content.slideDown(speed);
+			//scroll to element
+			$('html, body').animate({
+				scrollTop:  activeTab.offset().top - $('header').height()
+			}, speed);
+		}
+
+		// Bind the click event handler
+		$(this).find("a").click(function (e) {
+			if($(this).hasClass('active')) {
+				$content.slideDown({
+					scrollTop: $content.offset().top - $('header').height()
+				}, speed);
+				var screenSize = getScreenSize();
+				if (screenSize.width < 800) {
+					// scroll to element
+					$('html, body').animate({
+						scrollTop: $content.offset().top - $('header').height() + 300  // mobile
+					}, speed);
+				}else{
+					//scroll to element icons top
+					$('html, body').animate({
+						scrollTop:  $content.offset().top - $('header').height() + 300
+					}, speed);
+				}
+				e.preventDefault();
+				return false;
+			}
+			// Make the old tab inactive.
+			$active.removeClass('active');
+			// $content.slideUp({
+			// 	scrollTop: $content.offset().top - $('header').height() - $('.left_sidebar').height()
+			// }, speed);
+			$content.hide();
+
+			// Update the variables with the new link and content
+			$active = $(this);
+			$content = $(this.hash);
+
+			location.hash = $active[0].hash;
+
+			// Make the tab active.
+			$active.addClass('active');
+			$content.slideDown({
+				scrollTop: $content.offset().top - $('header').height()
+			}, speed);
+			var screenSize = getScreenSize();
+			if (screenSize.width < 800) {
+				// scroll to element
+				$('html, body').animate({
+					scrollTop: $content.offset().top - $('header').height() + 300 // mobile
+				}, speed);
+			}else{
+				//scroll to element icons top
+				$('html, body').animate({
+					scrollTop:  $content.offset().top - $('header').height() + 300
+				}, speed);
+			}
+
+			// Prevent the anchor\'s default click action
+			e.preventDefault();
+		});
 	});
 
-	$('.content-wrapper.news').after('<div id=\"myModal\" class=\"modal\">\n' +
-		'  <span class=\"close_modal\">&times;</span>\n' +
-		'  <img class=\"modal-content\" id=\"img01\">\n' +
-		'  <div id=\"caption\"></div>\n' +
-		'</div>');
 
-
-	// create references to the modal...
-	var modal = document.getElementById('myModal');
-// to all images -- note I'm using a class!
-	var images = document.getElementsByClassName('myImages');
-// the image in the modal
-	var modalImg = document.getElementById("img01");
-// and the caption in the modal
-	var captionText = document.getElementById("caption");
-
-// Go through all of the images with our custom class
-	for (var i = 0; i < images.length; i++) {
-		var img = images[i];
-		// and attach our click listener for this image.
-		img.onclick = function(evt) {
-			modal.style.display = "block";
-			modalImg.src = this.src;
-			captionText.innerHTML = this.alt;
-		}
-	}
-
-	var span = document.getElementsByClassName("close_modal")[0];
-
-	span.onclick = function() {
-		modal.style.display = "none";
-	}
+// 	// create references to the modal...
+// 	var modal = document.getElementById('myModal');
+// // to all images -- note I'm using a class!
+// 	var images = document.getElementsByClassName('myImages');
+// // the image in the modal
+// 	var modalImg = document.getElementById("img01");
+// // and the caption in the modal
+// 	var captionText = document.getElementById("caption");
+//
+// // Go through all of the images with our custom class
+// 	for (var i = 0; i < images.length; i++) {
+// 		var img = images[i];
+// 		// and attach our click listener for this image.
+// 		img.onclick = function(evt) {
+// 			modal.style.display = "block";
+// 			modalImg.src = this.src;
+// 			captionText.innerHTML = this.alt;
+// 		}
+// 	}
+//
+// 	var span = document.getElementsByClassName("close_modal")[0];
+//
+// 	span.onclick = function() {
+// 		modal.style.display = "none";
+// 	}
 
 });
 
@@ -146,17 +231,21 @@ function appendSignOut() {
     });
 }
 
-// function initAccordeon(pElem) {
-// 	$('body').on('click', '.accordion-toggle', function () {
-// 		if ($(this).next(".accordion-content").is(':visible')) {
-// 			$(this).next(".accordion-content").slideUp(300);
-// 			$(this).children(".plusminus").html('<span class="plus"></span>');
-// 		} else {
-// 			$(this).next(".accordion-content").slideDown(300);
-// 			$(this).children(".plusminus").html('<span class="minus"></span>');
-// 		}
-// 	});
-// }
+function appendSearchAndSocialMedia(){
+	var liSearch = '<li class="nav-item search_field"><a href=\"javascript: void(0);\" onclick=\"showSearchForm();\"></a></li>';
+	// var liSocial = '<li class="nav-item social"><a href=\"https://www.facebook.com/BiCIKLProjectH2020\" target=\"_blank\" class=\"pr p-facebook big\" target=\"_blank\"></a><a href=\"https://twitter.com/BiCIKL_H2020\" target=\"_blank\" class=\"pr p-twitter big\" target=\"_blank\"></a></li>';
+	var menu = $('#menuToggle');
+	// menu.find('>ul').append(liSearch).append(liSocial);
+	menu.find('>ul').append(liSearch);
+}
+
+function redirectAndRefresh(url){
+	$(".tabs a").each(function() {
+		this.href = window.location.hash;
+	});
+	window.open(url, '_blank');
+	location.reload();
+}
 
 function isBreakpointLarge() {
     return window.innerWidth <= 991;
@@ -199,6 +288,7 @@ function init() {
                 });
             }
         }
+		appendSearchAndSocialMedia()
         // keepFooter(documentHasScroll());
 
     });
